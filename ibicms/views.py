934dfe43028c20cbaba8d6107506c5f7577fd0a9
Mateusz Judziński <mateusz.judzinski@gmail.com>
2025-04-06 18:16:38 +0200

    small commit

--- ibicms/views.py
+++ ibicms/views.py
@@ -71,34 +71,34 @@ def business_creator(request):
 
 @login_required(login_url='/accounts/login/')
 def newsletter_creator(request):
-    # if request.method == 'POST':
-    #     title = request.POST.get('title')
-    #     hero_title = request.POST.get('hero_title')
-    #     hero_text = request.POST.get('hero_text')
-    #     # first_name = request.POST.get('first_name')
-    #     # last_name = request.POST.get('last_name')
-    #     phone = request.POST.get('phone')
-    #     email = request.POST.get('email')
-    #     project_text = request.POST.get('project_text')
-    #     user_photo = request.FILES.get('user_photo')
-    #     website_photo = request.FILES.get('website_photo')
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        hero_title = request.POST.get('hero_title')
+        hero_text = request.POST.get('hero_text')
+        # first_name = request.POST.get('first_name')
+        # last_name = request.POST.get('last_name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        project_text = request.POST.get('project_text')
+        user_photo = request.FILES.get('user_photo')
+        website_photo = request.FILES.get('website_photo')
 
-    #     ProfilePortfolio.objects.create(
-    #         user=request.user,
-    #         title=title,
-    #         hero_title=hero_title,
-    #         hero_text=hero_text,
-    #         # first_name=first_name,
-    #         # last_name=last_name,
-    #         phone=phone,
-    #         email=email,
-    #         project_text=project_text,
-    #         user_photo=user_photo,
-    #         website_photo=website_photo
-    #     )
-    #     return redirect('home')
+        ProfilePortfolio.objects.create(
+            user=request.user,
+            title=title,
+            hero_title=hero_title,
+            hero_text=hero_text,
+            # first_name=first_name,
+            # last_name=last_name,
+            phone=phone,
+            email=email,
+            project_text=project_text,
+            user_photo=user_photo,
+            website_photo=website_photo
+        )
+        return redirect('home')
 
-    # return render(request, 'creators/portfolio_creator.html')
+    return render(request, 'creators/newsletter_creator.html')
     pass
 
 def portfolio_preview(request, id):
